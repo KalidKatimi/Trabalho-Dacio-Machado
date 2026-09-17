4551f3ea01fd09c0078f770ad8e8eedd6e28d4fa
Kalid <kalidcairo@gmail.com>
2026-09-17 15:29:56 -0300

    Estrutua Inicial em C

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+    // Configura o terminal ou variáveis iniciais se necessário
+    
+    printf("=== PROJETO 1o BIMESTRE ===\n");
+
+    return 0;
+}
