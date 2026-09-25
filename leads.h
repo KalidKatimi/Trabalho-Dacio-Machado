d3f4a4640b126273ce9bd687e324b391883c4dab
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-09-24 23:16:17 -0300

    leads

---
+++ leads.h
@@ -0,0 +1,12 @@
+#define MAX_LEADS 50
+
+typedef struct {
+    int id;
+    char nome[50];
+    int origem;
+    int temperatura;
+} Lead;
+
+void cadastrarLead();
+void listarLeads();
+void buscarLead();
