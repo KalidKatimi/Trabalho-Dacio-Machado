21fe2fdfadc3fda24f02ef07589ec14e1f37a280
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-09-24 23:51:11 -0300

    O main do nosso sistema de leads

---
+++ main.c
@@ -0,0 +1,29 @@
+// main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "leads.h"
+
+int main() {
+    int opcao = 0;
+    do {
+        printf("\n========================================\n");
+        printf("       SISTEMA CRM (ARQUIVOS MULTIPLOS)   \n");
+        printf("========================================\n");
+        printf("1. Cadastrar novo Lead\n");
+        printf("2. Listar Leads (Ler do Ficheiro)\n");
+        printf("3. Buscar Lead por Nome\n");
+        printf("4. Sair do Sistema\n");
+        printf("========================================\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        
+        switch(opcao) {
+            case 1: cadastrarLead(); break;
+            case 2: listarLeads(); break;
+            case 3: buscarLead(); break;
+            case 4: printf("\nEncerrando...\n"); break;
+            default: printf("\n[ ERRO ] Opcao invalida.\n");
+        }
+    } while (opcao != 4);
+    return 0;
+}
