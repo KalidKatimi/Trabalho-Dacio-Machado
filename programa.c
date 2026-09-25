982c33885228f00fc039f56d44ac674eb9ffcfab
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-09-25 00:10:04 -0300

    Incrementei o system cls pra limpar a tela e ficar mais dinâmico

--- programa.c
+++ programa.c
@@ -14,6 +14,8 @@ typedef struct {
 void cadastrarLead() {
     Lead novoLead;
     FILE *arquivo;
+    
+    system("cls");
     printf("\n--- CADASTRAR LEAD ---\n");
     arquivo = fopen("dados_leads.txt", "a");
     if (arquivo == NULL) { 
@@ -27,22 +29,32 @@ void cadastrarLead() {
     
     fprintf(arquivo, "%d;%s;%d;%d\n", novoLead.id, novoLead.nome, novoLead.origem, novoLead.temperatura);
     fclose(arquivo);
+    
     printf("\n[ SUCESSO ] Lead guardado no ficheiro!\n");
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
 
 void listarLeads() {
     Lead leadAtual;
     FILE *arquivo;
+    
+    system("cls");
     printf("\n--- LISTA DE LEADS ---\n");
     arquivo = fopen("dados_leads.txt", "r");
     if (arquivo == NULL) { 
         printf("[ AVISO ] O ficheiro esta vazio.\n"); 
+        printf("\nPressione Enter para voltar ao menu...");
+        scanf(" %[^\n]");
         return; 
     }
     while (fscanf(arquivo, "%d;%[^;];%d;%d\n", &leadAtual.id, leadAtual.nome, &leadAtual.origem, &leadAtual.temperatura) != EOF) {
         printf("ID: %d | Nome: %s | Orig: %d | Temp: %d\n", leadAtual.id, leadAtual.nome, leadAtual.origem, leadAtual.temperatura);
     }
     fclose(arquivo);
+    
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
 
 void buscarLead() {
@@ -51,6 +63,7 @@ void buscarLead() {
     char termoBusca[50];
     int encontrou = 0;
     
+    system("cls");
     printf("\n--- BUSCAR LEAD ---\n");
     printf("Digite o nome exato para buscar: ");
     scanf(" %[^\n]s", termoBusca);
@@ -58,6 +71,8 @@ void buscarLead() {
     arquivo = fopen("dados_leads.txt", "r");
     if (arquivo == NULL) { 
         printf("[ ERRO ] Base de dados indisponivel.\n"); 
+        printf("\nPressione Enter para voltar ao menu...");
+        scanf(" %[^\n]");
         return; 
     }
     
@@ -74,11 +89,14 @@ void buscarLead() {
     }
     
     fclose(arquivo);
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
 
 int main() {
     int opcao = 0;
     do {
+        system("cls");
         printf("\n========================================\n");
         printf("       GERADOR DE LEADS (CRM)             \n");
         printf("========================================\n");
@@ -94,8 +112,14 @@ int main() {
             case 1: cadastrarLead(); break;
             case 2: listarLeads(); break;
             case 3: buscarLead(); break;
-            case 4: printf("\nEncerrando...\n"); break;
-            default: printf("\n[ ERRO ] Opcao invalida.\n");
+            case 4: 
+                system("cls");
+                printf("\nEncerrando o programa... Até logo!\n"); 
+                break;
+            default: 
+                printf("\n[ ERRO ] Opcao invalida.\n");
+                printf("Pressione Enter para continuar...");
+                scanf(" %[^\n]");
         }
     } while (opcao != 4);
     return 0;
