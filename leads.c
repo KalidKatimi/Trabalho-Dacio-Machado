0fb0d2125b9b7eb999bd8d8c2a57a9dfa5727bb4
João Pedro Peres Portela Garcia <pedrogarcya15@gmail.com>
2026-09-25 00:04:40 -0300

    Incrementei o system cls pra limpar a tela e ficar mais dinâmico

--- leads.c
+++ leads.c
@@ -7,6 +7,8 @@
 void cadastrarLead() {
     Lead novoLead;
     FILE *arquivo;
+    
+    system("cls");
     printf("\n--- CADASTRAR LEAD ---\n");
     arquivo = fopen("dados_leads.txt", "a");
     if (arquivo == NULL) { 
@@ -20,22 +22,32 @@ void cadastrarLead() {
     
     fprintf(arquivo, "%d;%s;%d;%d\n", novoLead.id, novoLead.nome, novoLead.origem, novoLead.temperatura);
     fclose(arquivo);
+    
     printf("\n[ SUCESSO ] Lead guardado no ficheiro!\n");
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
 
 void listarLeads() {
     Lead leadAtual;
     FILE *arquivo;
+    
+    system("cls");
     printf("\n--- LISTA DE LEADS ---\n");
     arquivo = fopen("dados_leads.txt", "r");
     if (arquivo == NULL) { 
         printf("[ AVISO ] O ficheiro esta vazio.\n"); 
+        printf("\nPressione Enter para voltar ao menu...");
+        scanf(" %[^\n]");
         return; 
     }
     while (fscanf(arquivo, "%d;%[^;];%d;%d\n", &leadAtual.id, leadAtual.nome, &leadAtual.origem, &leadAtual.temperatura) != EOF) {
         printf("ID: %d | Nome: %s | Orig: %d | Temp: %d\n", leadAtual.id, leadAtual.nome, leadAtual.origem, leadAtual.temperatura);
     }
     fclose(arquivo);
+    
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
 
 void buscarLead() {
@@ -44,6 +56,7 @@ void buscarLead() {
     char termoBusca[50];
     int encontrou = 0;
     
+    system("cls");
     printf("\n--- BUSCAR LEAD ---\n");
     printf("Digite o nome exato para buscar: ");
     scanf(" %[^\n]s", termoBusca);
@@ -51,6 +64,8 @@ void buscarLead() {
     arquivo = fopen("dados_leads.txt", "r");
     if (arquivo == NULL) { 
         printf("[ ERRO ] Base de dados indisponivel.\n"); 
+        printf("\nPressione Enter para voltar ao menu...");
+        scanf(" %[^\n]");
         return; 
     }
     
@@ -62,10 +77,11 @@ void buscarLead() {
         }
     }
     
-    // Tratamento de erro obrigatorio caso nao encontre resultados
     if (encontrou == 0) {
         printf("\n[ AVISO ] A busca nao retornou resultados. Lead nao encontrado.\n");
     }
     
     fclose(arquivo);
+    printf("\nPressione Enter para voltar ao menu...");
+    scanf(" %[^\n]");
 }
